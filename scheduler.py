a3bcbb44c2d1f76285682cbfdf5edd84f51d9d19
CameronBWelsh <cameronbrianwelsh@gmail.com>
2026-07-12 21:06:01 -0400

    Added limitations to water alerts so it doesnt continue to alert every 10 seconds

--- scheduler.py
+++ scheduler.py
@@ -4,9 +4,13 @@ import time
 import requests
 
 counter = 0
+alert_counter = 0
 while True: 
-    if sensor.read_water():
+    if sensor.read_water() and alert_counter < 3:
         requests.post("https://ntfy.sh/welsh-basement-alarm-631621", data="Water detected in basement!")
+        alert_counter += 1
+    elif not sensor.read_water() and alert_counter > 0:
+        alert_counter = 0
 
     counter += 1
     if counter >= 60:
